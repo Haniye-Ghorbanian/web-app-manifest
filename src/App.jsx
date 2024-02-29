9483e055f2fb20b8d1635d87ce28194b60b5d613
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-29 12:23:13 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return(
+    <div className='h-screen w-full bg-blue-900 flex items-center justify-center text-white font-extrabold'>
+      <h1>Dummy Progressive Web App</h1>
+    </div>
+  )
+}
+
+export default App
