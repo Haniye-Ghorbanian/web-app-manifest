6f5d776fbeaef4bc022c16a154057d9ede0f9fb6
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-29 12:30:21 +0330

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@
 
 function App() {
   return(
-    <div className='h-screen w-full bg-blue-900 flex items-center justify-center text-white font-extrabold'>
-      <h1>Dummy Progressive Web App</h1>
+    <div className='h-screen w-full bg-blue-900 flex flex-col items-center justify-center text-white'>
+      <h1 className="font-extrabold text-2xl mb-8">Dummy Progressive Web App</h1>
+
+      <p className="font-light text-md">Nothing to show just for presentation</p>
     </div>
   )
 }
